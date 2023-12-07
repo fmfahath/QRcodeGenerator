ef8965face041af93a24805f22a2592d933c5e2e
fmfahath <fmfahath.dev@gmail.com>
2023-12-07 15:04:26 +0530

    js : input validation added, css shake animationadded

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const imgBoxEl = document.getElementById('imgBox');
 const qrImgEl = document.getElementById('qrImg');
 const api_url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
 
-function gnerateQR(){
-    console.log(qrTextEl.value);
-    qrImgEl.src = api_url + qrTextEl.value;
-    console.log(qrImgEl.src);
-    imgBoxEl.classList.add('show-imgBox');
+function gnerateQR() {
+    if (qrTextEl.value.length > 0) {
+        qrImgEl.src = api_url + qrTextEl.value;
+        imgBoxEl.classList.add('show-imgBox');
+        // console.log(qrImgEl.src);
+    }
+    else{
+        qrTextEl.classList.add('error');
+        setTimeout(() => {
+            qrTextEl.classList.remove('error');
+        }, 1000);
+    }
 }
+
 
