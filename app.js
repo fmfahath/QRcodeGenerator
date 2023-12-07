d422b9b5cbac9f6355b3b83433d59951b1f30dcd
fmfahath <fmfahath.dev@gmail.com>
2023-12-07 14:31:25 +0530

    js : QR api function  added

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const qrTextEl = document.getElementById('qrText');
+const imgBoxEl = document.getElementById('imgBox');
+const qrImgEl = document.getElementById('qrImg');
+const api_url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+
+function gnerateQR(){
+    qrImgEl.src = api_url + qrTextEl.value;
+    // console.log(qrImgEl.src);
+}
